c41c6f3814b115aadc61cbed67ff64bedcea34e7
antoniouaa <antoniouaa@hotmail.com>
2021-07-16 18:01:10 +0100

    testing git info

--- krypto/__init__.py
+++ krypto/__init__.py
@@ -1,15 +1,16 @@
 import os
 import pathlib
 
+__version__ = "0.1.0"
+
 import click
 
 token = os.getenv("TOKEN_GITHUB")
 assert token is not None
 
-__version__ = "0.1.0"
-
 from krypto.todo import parse
 from krypto.github import create_issues
+
 
 # TODO[Enhancement]: Make config file
 # Sometimes you might wanna have TODOs in your tests.
@@ -17,13 +18,11 @@ from krypto.github import create_issues
 # substring "test" in the path. I would want to be able to
 # configure this behaviour.
 
-
 # TODO[Enhancement, Bug]: Allow for different standard labels to be attached to the issues
 # Mayhap I would like to assign a todo certain labels like `Enhancement`
 # Possible syntax `# Enhancement: title here`
 # or maybe `# TODO[Enhancement]: title here`
 # Labels are overwritten right now instead of extended
-
 
 # TODO: Possible creation of pre-push git hook
 # Think about making this into a git script to

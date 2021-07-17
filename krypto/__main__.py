fe06a52cc90e250608054a205d10a9217ab7a2c5
antoniouaa <antoniouaa@hotmail.com>
2021-07-17 21:57:11 +0100

    removed todo about prepost hook

--- krypto/__main__.py
+++
@@ -1,4 +0,0 @@
-# TODO[Enhancement]: Possible creation of pre-push git hook - https://github.com/antoniouaa/krypto/issues/33
-# Think about making this into a git script to
-# hook into the pre-push action instead of having to run
-# the script manually

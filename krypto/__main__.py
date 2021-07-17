21b4cea1199f71bdb56163af82f3ae108431413b
antoniouaa <antoniouaa@hotmail.com>
2021-07-17 23:32:33 +0100

    testing pre-push hook

---
+++ krypto/__main__.py
@@ -0,0 +1,4 @@
+# TODO[Enhancement]: Possible creation of pre-push git hook
+# Think about making this into a git script to hook into the pre-push action
+# instead of having to run the script manually
+# Lets try to test this right now
